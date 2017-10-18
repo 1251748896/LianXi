aa48db92016dcea54fbcb7da8dac5c2a0e6eccc0
jiangbo <1251748896@qq.com>
2017-10-18 16:56:27 +0800

    222
    2222

---
+++ LianXi/AppDelegate.h
@@ -0,0 +1,111 @@
+//
+//  AppDelegate.h
+//  LianXi
+//
+//  Created by Mac on 17/4/10.
+//  Copyright © 2017年 jiangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
+
+//  $(SRCROOT)/LianXi/基类/PrefixHeader.pch
+
+// 我们可以将目录中的Code Snippets备份，也可以将其直接拷出来放在不同的电脑上使用，因此多台电脑之间的协作也毫无压力。
+// Code Snippets的备份
+// Xcode中的Code Snippets默认放在下面的目录中
+// ~/Library/Developer/Xcode/UserData/CodeSnippets
+/*
+ 
+ //Xcode中的当中的，可以真机测试的，手机系统目录路径
+ /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport
+ 
+ */
+
+
+/*
+ 
+ 403:访问被拒绝
+ 404:参数错误
+ 405:请求方法的类型错误
+ 
+ 开发者账号:yudeng@sina.com
+ 密码:Scyd20160808
+ 
+ apple ID 的相关信息
+ 
+ {
+ 
+ appleid
+ yudengtouzi@sina.com
+ Scyd20160808
+ 
+ 新浪邮箱
+ yudengtouzi@sina.com
+ Qwe20160808
+ 
+ 验证生日:
+ 1990/06/06
+ 
+ 验证问题:
+ (1).what's your dream jobs?
+     iosDeveloper
+ (2).what was the first name of your first boss?
+     myself
+ (3).what is the last name of your favorite elementary school teacher?
+     laoma
+ 
+ 
+ 四川宇登投资咨询有限公司
+ SiChuan Yu Deng TouZi Co., Ltd.
+ 四川省成都市二环路南四段51号
+ Second Ring Road South 4th part No.51, Chengdu, Sichuan
+ 
+ number : 5444424470
+ Website: www.ydtz360.com
+ iphone : 15982285748
+ 
+ 
+ ps:
+ 以下是需要核实的信息：
+ 公司的注册名字——中英文：SiChuan Yu Deng TouZi Ltd (四川宇登投资咨询有限公司)
+ 公司若更过注册名字，请写出之前名字：
+ 公司的办公地址及邮编（邮编请写详细的）——中英文：四川省成都市二环路南四段51号
+ 公司电话：15982285748
+ 公司有无分公司或办事处：没有分公司或办事处
+ 公司有无英文缩写或简称，有请写出：
+ 公司企业性质（营业执照上的公司类型）：有限责任公司（自然人投资或控股）
+ 公司大致的员工人数：50~100人
+ 公司主营业务(一个最主要的业务)：抵押，质押
+ 苹果项目联系人的名字，职务，性别，联系电话：姜波，ios开发，男，15982285748
+ 
+ }
+ 
+ */
+
+
+/*
+ 
+ 姓名: 姜波
+ 日期: 2017/7/3
+ 
+ [工作日志]
+ -------------------------------------------------------------
+ 1、工作内容一  进度:[已完成]
+ 
+ (1).优化了密码的判断方法
+ (2).修改了原来的利率显示方式
+ 
+ [明日工作计划]
+ 1、工作内容一
+ 
+ (1).测试app
+ 
+ */
